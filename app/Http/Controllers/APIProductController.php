945feb909da185cf0a904b6615c7e4a09c1179b2
TuanHung24 <tuanhung2401003@gmail.com>
2024-06-24 19:17:14 +0700

    update logo

--- app/Http/Controllers/APIProductController.php
+++ app/Http/Controllers/APIProductController.php
@@ -3,103 +3,105 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\ProductDetail;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+
+use function Laravel\Prompts\select;
 
 class APIProductController extends Controller
 {
     public function listProduct()
     {
-        
+
         $listProduct = Product::whereHas('product_detail', function ($query) {
 
             $query->where('quantity', '>', 0);
         })
-        ->with([
-            'brand',
-            'img_product',
-            'product_series',
-            'rate',
-            'product_detail.color',
-            'product_detail.capacity',
-            'product_detail.discount_detail'=>function($query){
-                $query->isActive();
-            }
-        ])
-        ->get();
-        
-        
+            ->with([
+                'brand',
+                'img_product',
+                'product_series',
+                'rate',
+                'product_description',
+                'product_detail.color',
+                'product_detail.capacity',
+                'product_detail.discount_detail' => function ($query) {
+                    $query->isActive();
+                }
+            ])
+            ->get();
+
+
         return response()->json([
-            'success' =>true,
-            'data'=>$listProduct
+            'success' => true,
+            'data' => $listProduct
         ]);
     }
     public function getProductDetail($id)
     {
-        try{
+        try {
             $proDuct = Product::whereHas('product_detail', function ($query) {
                 $query->where('quantity', '>', 0);
             })
-            ->with([
-                'product_description.front_camera',
-                'product_description.rear_camera',
-                'product_description.screen',
-                'rate',
-                'comment.customer' => function($query) {
-                    $query->select('id', 'name');
-                },
-                'comment.comment_detail.admin' => function($query) {
-                    $query->select('id', 'name');
-                },
-                'brand',
-                'product_series',
-                'img_product', 
-                'product_detail.color', 
-                'product_detail.capacity',
-                'product_detail.discount_detail' => function($query) {
-                    $query->isActive()->with(['discount' => function($query) {
-                        $query->select('id', 'date_start', 'date_end'); // Chọn cột 'date_start' và 'date_end' từ bảng 'discount'
-                    }]);
-                }
-            ])
-            ->findOrFail($id);
-        if(empty($proDuct))
-        {
+                ->with([
+                    'product_description.front_camera',
+                    'product_description.rear_camera',
+                    'product_description.screen',
+                    'rate',
+                    'comment.customer' => function ($query) {
+                        $query->select('id', 'name');
+                    },
+                    'comment.comment_detail.admin' => function ($query) {
+                        $query->select('id', 'name');
+                    },
+                    'brand',
+                    'product_series',
+                    'img_product',
+                    'product_detail.color',
+                    'product_detail.capacity',
+                    'product_detail.discount_detail' => function ($query) {
+                        $query->isActive()->with(['discount' => function ($query) {
+                            $query->select('id', 'date_start', 'date_end'); // Chọn cột 'date_start' và 'date_end' từ bảng 'discount'
+                        }]);
+                    }
+                ])
+                ->findOrFail($id);
+            if (empty($proDuct)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => "Sản phẩm ID {$id} không tồn tại"
+                ]);
+            }
             return response()->json([
-                'success' =>false,
-                'message'=>"Sản phẩm ID {$id} không tồn tại"
+                'success' => true,
+                'data' => $proDuct
             ]);
-        }
-        return response()->json([
-            'success' =>true,
-            'data'=>$proDuct
-        ]);
-        }catch(ModelNotFoundException $e){
+        } catch (ModelNotFoundException $e) {
             return response()->json([
                 'success' => false,
                 'message' => "Sản phẩm ID {$id} không tồn tại"
             ]);
         }
     }
-    
-    public function timKiem(Request $request )
+
+    public function timKiem(Request $request)
     {
-        $proDuct=Product::with(['brand','product_series'])->where('name',$request->name)->first();
-        if(empty($proDuct))
-        {
+        $proDuct = Product::with(['brand', 'product_series'])->where('name', $request->name)->first();
+        if (empty($proDuct)) {
             return response()->json([
-                'success'=>false,
-                'message'=>"Sản phẩm tên {$request->name} không tồn tại"
-            ]); 
+                'success' => false,
+                'message' => "Sản phẩm tên {$request->name} không tồn tại"
+            ]);
         }
         return response()->json([
-            'success'=>true,
-            'data'=>$proDuct
+            'success' => true,
+            'data' => $proDuct
         ]);
-    } 
+    }
     public function timKiemTen($searchTerm)
     {
-        $name=$searchTerm;
+        $name = $searchTerm;
         $proDuct = Product::with([
             'brand',
             'product_series',
@@ -107,17 +109,42 @@ class APIProductController extends Controller
             'product_detail' => function ($query) {
                 $query->with('color', 'capacity');
             }
-        ])->where('name', 'like', '%'. $name . '%')->get();
-        if($proDuct->isEmpty())
-        {
+        ])->where('name', 'like', '%' . $name . '%')->get();
+        if ($proDuct->isEmpty()) {
             return response()->json([
-                'success'=>false,
-                'message'=>"Sản phẩm không tồn tại"
-            ]); 
+                'success' => false,
+                'message' => "Sản phẩm không tồn tại"
+            ]);
         }
         return response()->json([
-            'success'=>true,
-            'data'=>$proDuct
+            'success' => true,
+            'data' => $proDuct
+        ]);
+    }
+    public function setPrice($searchTerm, $priceRange)
+    {
+        $name = $searchTerm;
+        list($priceMin, $priceMax) = explode('-', $priceRange);
+
+        $productDetail = ProductDetail::with([
+            'img_product',
+            'color',
+            'capacity',
+            'product' => function ($query) use ($name) {
+                $query->select('id', 'name')->where('name', 'like', '%' . $name . '%');
+            }
+        ])->whereBetween('price', [(int)$priceMin, (int)$priceMax])->get();
+
+        if ($productDetail->isEmpty()) {
+            return response()->json([
+                'success' => false,
+                'message' => "Sản phẩm không tồn tại"
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $productDetail
         ]);
     }
 }
